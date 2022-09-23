4728597328ee10bd0d71416f4f5f7a003cafaf23
Baodi Shi <wudixiaolongbao@icloud.com>
2022-09-23 16:57:49 +0800

    Only work lock when subType != shared

--- pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerImpl.java
+++ pulsar-client/src/main/java/org/apache/pulsar/client/impl/ConsumerImpl.java
@@ -1875,7 +1875,8 @@ public class ConsumerImpl<T> extends ConsumerBase<T> implements ConnectionHandle
             }
 
             int currentSize;
-            synchronized (incomingQueueLock) {
+            incomingQueueLock.lock();
+            try {
                 // we should increase epoch every time, because MultiTopicsConsumerImpl also increase it,
                 // we need to keep both epochs the same
                 if (conf.getSubscriptionType() == SubscriptionType.Failover
@@ -1887,7 +1888,8 @@ public class ConsumerImpl<T> extends ConsumerBase<T> implements ConnectionHandle
                 currentSize = incomingMessages.size();
                 clearIncomingMessages();
                 unAckedMessageTracker.clear();
-
+            } finally {
+                incomingQueueLock.unlock();
             }
 
             // is channel is connected, we should send redeliver command to broker
